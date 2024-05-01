d17c293d7e0fcd30852baa4c96c088dd0572f6ce
Martin Zikmund <martin@zikmund.dev>
2024-05-01 19:18:18 +0200

    feat: WASM TextBlock SelectAll and CopySelectionToClipboard

--- src/Uno.UI/UI/Xaml/Controls/TextBlock/TextBlock.skia.cs
+++ src/Uno.UI/UI/Xaml/Controls/TextBlock/TextBlock.skia.cs
@@ -332,6 +332,9 @@ namespace Microsoft.UI.Xaml.Controls
 			}
 		}
 
+		/// <summary>
+		/// Copies the selected content to the Windows clipboard.
+		/// </summary>
 		public void CopySelectionToClipboard()
 		{
 			if (Selection.start != Selection.end)
@@ -345,9 +348,7 @@ namespace Microsoft.UI.Xaml.Controls
 			}
 		}
 
-		public void SelectAll() => Selection = new Range(0, Text.Length);
-
-		// TODO: move to TextBlock.cs when we implement SelectionHighlightColor for the other platforms
+		// TODO: move to TextBlock.cs when we implement SelectionHighlightColor for the other platform
 		public SolidColorBrush SelectionHighlightColor
 		{
 			get => (SolidColorBrush)GetValue(SelectionHighlightColorProperty);
