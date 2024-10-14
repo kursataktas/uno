b623c74781ba06a1703e271f8f2c648bba7344f1
Youssef Victor <youssefvictor00@gmail.com>
2024-10-14 09:40:21 +0300

    chore: Fix mobile build

--- src/Uno.UI/UI/Xaml/Controls/CalendarView/CalendarViewBaseItemChrome.cs
+++ src/Uno.UI/UI/Xaml/Controls/CalendarView/CalendarViewBaseItemChrome.cs
@@ -284,7 +284,7 @@ namespace Microsoft.UI.Xaml.Controls
 			}
 
 			// Get the child to measure it - if any.
-			View pChildNoRef = GetFirstChildNoAddRef();
+			var pChildNoRef = GetFirstChildNoAddRef() as UIElement;
 
 			//If we have a child
 			if (pChildNoRef is { })
@@ -361,7 +361,7 @@ namespace Microsoft.UI.Xaml.Controls
 				m_pLabelTextBlock.Arrange(finalBounds);
 			}
 
-			View pChildNoRef = GetFirstChildNoAddRef();
+			var pChildNoRef = GetFirstChildNoAddRef() as UIElement;
 
 			if (pChildNoRef is { })
 			{
